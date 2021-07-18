029b0c7fee912a3e1bf359d2b33c8d64e9afe696
Joel Gimenez <joelsinbarba@gmail.com>
2021-07-18 17:45:58 -0400

    Solucionar cors

--- src/main/java/com/uaa/gestiondealmacen/GestiondealmacenApplication.java
+++ src/main/java/com/uaa/gestiondealmacen/GestiondealmacenApplication.java
@@ -30,7 +30,7 @@ public class GestiondealmacenApplication extends SpringBootServletInitializer {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
+				registry.addMapping("/**").allowedMethods("*");
 			}
 		};
 	}
